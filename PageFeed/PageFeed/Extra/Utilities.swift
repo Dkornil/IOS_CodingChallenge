c2b135475301b34b17aafa084a647f22b1b02bf5
dkornil <dkornilo1@gmail.com>
2021-10-01 12:37:13 -0400

    Main Application
    Created the App

---
+++ PageFeed/PageFeed/Extra/Utilities.swift
@@ -0,0 +1,44 @@
+//
+//  Utilities.swift
+//  PageFeed
+//
+//  Created by Field Employee on 10/1/21.
+//
+
+import Foundation
+import UIKit
+
+class Utilities {
+    
+    func getTextAndImageForComment(comments:String?) -> NSAttributedString {
+        let fullString = NSMutableAttributedString(string: "Comments: \(comments ?? "")  ")
+
+        let commentsImage = NSTextAttachment()
+
+        commentsImage.image = UIImage(systemName: "bubble.right.fill", withConfiguration:.none)
+        
+        let commentsAndString = NSAttributedString(attachment: commentsImage)
+
+        fullString.append(commentsAndString)
+
+        return fullString
+    }
+
+    func getTextAndImageForScore(score:String?) -> NSAttributedString {
+        let fullString = NSMutableAttributedString(string: "\(score ?? "") ")
+
+
+        let scoreImage = NSTextAttachment()
+        //let sImage = NSTextAttachment()
+        
+
+        scoreImage.image = UIImage(systemName: "arrow.up.arrow.down", withConfiguration:.none)
+        
+        
+        let scoreAndImage = NSAttributedString(attachment: scoreImage)
+        
+        fullString.append(scoreAndImage)
+
+        return fullString
+     }
+}
---
+++ PageFeed/PageFeed/Model/FeedModel.swift
@@ -0,0 +1,31 @@
+//
+//  FeedModel.swift
+//  PageFeed
+//
+//  Created by Field Employee on 10/1/21.
+//
+
+import Foundation
+
+struct Feed: Codable {
+    let data: FeedData
+}
+
+struct FeedData: Codable {
+    let after: String
+    let children: [Child]
+}
+
+struct Child: Codable {
+    let data: ChildData
+}
+
+struct ChildData: Codable {
+    let title: String?
+    let num_comments: Int
+    let score: Int
+    let thumbnail: String?
+    let thumbnail_height: Int?
+    let thumbnail_width: Int?
+    
+}
